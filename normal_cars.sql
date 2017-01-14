53d71b37c22f5fae4bd95a5093b19def86abce5e
Tyler Nichols <tylernchls@gmail.com>
2017-01-13 19:42:30 -1000

    created new table and pulled data from old table

---
+++ normal_cars.sql
@@ -0,0 +1,21 @@
+DROP USER IF EXISTS normal_user;
+
+CREATE USER normal_user;
+
+DROP DATABASE IF EXISTS normal_cars;
+
+CREATE DATABASE normal_cars WITH OWNER normal_user;
+
+DROP TABLE IF EXISTS make_table;
+
+CREATE TABLE make_table (
+  make_id serial NOT NULL primary key,
+  make_code character varying(125) NOT NULL,
+  make_title character varying(125) NOT NULL
+  );
+
+\c normal_cars normal_user
+\i scripts/denormal_data.sql
+
+INSERT INTO make_table(make_code, make_title)
+  SELECT DISTINCT make_code, make_title FROM car_models;
